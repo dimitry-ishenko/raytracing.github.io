3e7916eae030d53050d904f16033dc509f0b8f85
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2024-01-24 22:10:28 -0500

    color3: conversion from vec3

--- color.hpp
+++ color.hpp
@@ -2,12 +2,15 @@
 #define COLOR_HPP
 
 #include "array.hpp"
+#include "vec.hpp"
 
 struct color3 : array<double, 3>
 {
     constexpr auto r() const { return data[0]; }
     constexpr auto g() const { return data[1]; }
     constexpr auto b() const { return data[2]; }
+
+    static auto from_vec3(const vec3& v) { return color3{v.x(), v.y(), v.z()}; }
 };
 
 #endif
