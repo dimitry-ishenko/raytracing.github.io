ea8e1ed6878f8410227b4bea60b0853d018434c6
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2024-02-13 01:29:47 -0500

    Fuzzy reflection
    Using formula from wikipedia.

--- metal.hpp
+++ metal.hpp
@@ -4,17 +4,32 @@
 #include "color.hpp"
 #include "material.hpp"
 #include "object.hpp" // hit
+#include "random.hpp"
+#include "vec.hpp"
 
 struct metal : material
 {
     color3 albedo;
+    double fuzz;
 
-    explicit metal(const color3& albedo) : albedo{albedo} { }
+    metal(const color3& albedo, double fuzz) : albedo{albedo}, fuzz{fuzz} { }
 
-    virtual std::optional<scatter> get_scatter(const ray3& ray_in, const hit& hit) const override
+    virtual std::optional<scatter> get_scatter(const ray3& ray, const hit& hit) const override
     {
-        auto dir = ray_in.dir - 2 * dot(ray_in.dir, hit.norm) * hit.norm; // reflect
+        static rnd_sphere3_gen rnd{1};
+
+        auto dir = reflect(unit{ray.dir}, hit.norm) + fuzz * rnd();
+        if (dot(dir, hit.norm) < 0) return { };
+
         return scatter{ ray3{hit.point, dir}, albedo };
+    }
+
+private:
+    static vec3 reflect(const unit<vec3>& l, const unit<vec3>& n)
+    {
+        // https://en.wikipedia.org/wiki/Snell%27s_law#Vector_form
+        auto cos_th1 = dot(-n, l);
+        return l + 2 * cos_th1 * n;
     }
 };
 
