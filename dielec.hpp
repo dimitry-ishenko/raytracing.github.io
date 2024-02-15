37ea88cd1ea7e8e8342ad00bf20cc3cfb687881d
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2024-02-15 01:20:35 -0500

    Total internal reflection

--- dielec.hpp
+++ dielec.hpp
@@ -6,8 +6,6 @@
 #include "material.hpp"
 #include "vec.hpp"
 
-#include <cmath> // std::abs
-
 struct dielec : material
 {
     double ri;
@@ -16,16 +14,21 @@ struct dielec : material
 
     virtual std::optional<scatter> get_scatter(const ray3& ray, const hit& hit) const override
     {
-        auto dir = refract(unit{ray.dir}, hit.norm, hit.front ? 1 / ri : ri);
+        auto dir = refract_or_reflect(unit{ray.dir}, hit.norm, hit.front ? 1 / ri : ri);
         return scatter{ ray3{hit.point, dir}, color3{1, 1, 1} };
     }
 
 private:
-    static vec3 refract(const unit<vec3>& l, const unit<vec3>& n, double ri)
+    static vec3 refract_or_reflect(const unit<vec3>& l, const unit<vec3>& n, double ri)
     {
         // https://en.wikipedia.org/wiki/Snell%27s_law#Vector_form
         auto cos_th1 = dot(-n, l);
-        auto cos_th2 = std::sqrt(std::abs(1 - ri * ri * (1 - cos_th1 * cos_th1)));
+        auto sin2_th2 = ri * ri * (1 - cos_th1 * cos_th1);
+
+        if (sin2_th2 > 1) return l + 2 * cos_th1 * n; // must reflect
+
+        // can refract
+        auto cos_th2 = std::sqrt(1 - sin2_th2);
         return ri * l + (ri * cos_th1 - cos_th2) * n;
     }
 };
--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@
 int main(int argc, char* argv[])
 {
     auto mat_ground = std::make_shared<lambert>(color3{.8, .8,  0});
-    auto mat_center = std::make_shared<dielec >(1.5);
+    auto mat_center = std::make_shared<lambert>(color3{.1, .2, .5});
     auto mat_left   = std::make_shared<dielec >(1.5);
-    auto mat_right  = std::make_shared<metal  >(color3{.8, .6, .2},  1);
+    auto mat_right  = std::make_shared<metal  >(color3{.8, .6, .2}, 0);
 
     object_list world;
     world.children.emplace_back(new sphere3{ point3{ 0, -100.5, -1}, 100, mat_ground });
