6a32953bd4990b5cca85e32255edb3584b881469
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2024-02-25 22:20:04 -0500

    interval: formatting

--- interval.hpp
+++ interval.hpp
@@ -15,14 +15,12 @@ constexpr auto len(const interval& in) { return in.max - in.min; }
 
 constexpr auto merge(const interval& ix, const interval& iy)
 {
-    auto min = std::min(ix.min, iy.min), max = std::max(ix.max, iy.max);
-    return interval{min, max};
+    return interval{ std::min(ix.min, iy.min), std::max(ix.max, iy.max) };
 }
 
 constexpr auto pad(const interval& in, double p)
 {
-    auto min = in.min - p / 2, max = in.max + p / 2;
-    return interval{min, max};
+    return interval{ in.min - p / 2, in.max + p / 2 };
 }
 
 constexpr auto surr(const interval& in, double t) { return in.min < t && t < in.max; }
