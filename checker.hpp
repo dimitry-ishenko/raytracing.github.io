03aa45173ad57fe4fa0b4871c17c692e7bef4361
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2024-02-28 18:32:40 -0500

    Add checker

---
+++ checker.hpp
@@ -0,0 +1,35 @@
+#ifndef CHECKER_HPP
+#define CHECKER_HPP
+
+#include "color.hpp"
+#include "point.hpp"
+#include "solid.hpp"
+#include "texture.hpp"
+
+#include <cmath> // std::floor
+
+struct checker : texture
+{
+    double scale;
+    shared_texture even, odd;
+
+    checker(double scale, color3 even, color3 odd) :
+        scale{scale}, even{std::make_shared<solid>(std::move(even))}, odd{std::make_shared<solid>(std::move(odd))}
+    { }
+
+    checker(double scale, shared_texture even, shared_texture odd) :
+        scale{scale}, even{std::move(even)}, odd{std::move(odd)}
+    { }
+
+    virtual color3 value(const point2& uv, const point3& p) const override
+    {
+        int ix = std::floor(p.x() / scale);
+        int iy = std::floor(p.y() / scale);
+        int iz = std::floor(p.z() / scale);
+
+        auto s = (ix + iy + iz) % 2;
+        return s ? even->value(uv, p) : odd->value(uv, p);
+    }
+};
+
+#endif
