47464de2c5bc0d6eaef99ffb08829a5f9b6d233a
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2024-02-28 18:37:14 -0500

    lambert: use textures

--- lambert.hpp
+++ lambert.hpp
@@ -6,19 +6,22 @@
 #include "material.hpp"
 #include "random.hpp"
 #include "ray.hpp"
+#include "solid.hpp"
+#include "texture.hpp"
 
 struct lambert : material
 {
-    color3 albedo;
+    shared_texture albedo;
 
-    explicit lambert(const color3& albedo) : albedo{albedo} { }
+    explicit lambert(color3 albedo) : albedo{std::make_shared<solid>(std::move(albedo))} { }
+    explicit lambert(shared_texture albedo) : albedo{std::move(albedo)} { }
 
     virtual optional_scatter get_scatter(const ray3& ray, const hit& hit) const override
     {
         auto dir = hit.norm + unit(rnd_sphere3());
         if (near_0(dir)) dir = hit.norm;
 
-        return scatter{ ray3{hit.point, dir, ray.time}, albedo };
+        return scatter{ ray3{hit.point, dir, ray.time}, albedo->value(hit.uv, hit.point) };
     }
 };
 
