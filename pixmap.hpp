40eb688533ba30ca8cfd7843348e5dae051d2b3f
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2024-02-29 19:00:49 -0500

    Add pixmap texture

---
+++ pixmap.hpp
@@ -0,0 +1,28 @@
+#ifndef PIXMAP_HPP
+#define PIXMAP_HPP
+
+#include "color.hpp"
+#include "image.hpp"
+#include "point.hpp"
+#include "texture.hpp"
+
+#include <algorithm> // std::clamp
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+struct pixmap : texture
+{
+    struct image image;
+
+    explicit pixmap(const fs::path& path) : image{image::read_from(path)} { }
+
+    virtual color3 value(const point2& uv, const point3&) const override
+    {
+        int x = image.width * uv.x();
+        int y = image.height * (1 - uv.y());
+        return image.pixel[y * image.width + x];
+    }
+};
+
+#endif
