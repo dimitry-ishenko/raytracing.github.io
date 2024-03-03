48bce4c4761e9f87fb3abcf86cd9e82bd2dd5921
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2024-03-02 21:03:50 -0500

    Add Perlin noise texture

---
+++ noise.hpp
@@ -0,0 +1,18 @@
+#ifndef NOISE_HPP
+#define NOISE_HPP
+
+#include "color.hpp"
+#include "perlin.hpp"
+#include "texture.hpp"
+
+struct noise : texture
+{
+    virtual color3 value(const point2&, const point3& p) const override
+    {
+        return color3{1, 1, 1} * gen.noise(p);
+    }
+
+    perlin gen;
+};
+
+#endif
