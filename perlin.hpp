ac693a0e23b86909e57f1eb746e05bde333a83d9
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2024-03-03 22:03:39 -0500

    Using random vectors on the lattice points

--- perlin.hpp
+++ perlin.hpp
@@ -3,6 +3,7 @@
 
 #include "point.hpp"
 #include "random.hpp"
+#include "vec.hpp"
 
 #include <iterator>
 #include <numeric>
@@ -16,7 +17,7 @@ struct perlin
             std::iota(std::begin(p), std::end(p), 0);
             for (auto i = std::size(p) - 1; i > 0; --i) std::swap(p[i], p[ std::size_t(rnd() * i) ]);
         }
-        for (auto& n : rand) n = rnd();
+        for (auto& n : rand) n = unit(rnd_vec3());
     }
 
     auto noise(const point3& p) const
@@ -25,24 +26,27 @@ struct perlin
         int y = std::floor(p.y());
         int z = std::floor(p.z());
 
-        auto u = p.x() - x, v = p.y() - y, w = p.z() - z;
-        u = u * u * (3 - 2 * u);
-        v = v * v * (3 - 2 * v);
-        w = w * w * (3 - 2 * w);
+        auto u = p.x() - x; auto uu = u * u * (3 - 2 * u);
+        auto v = p.y() - y; auto vv = v * v * (3 - 2 * v);
+        auto w = p.z() - z; auto ww = w * w * (3 - 2 * w);
 
-        double acc = 0;
+        auto acc = 0.;
         for (auto i = 0; i < 2; ++i)
             for (auto j = 0; j < 2; ++j)
                 for (auto k = 0; k < 2; ++k)
-                    acc += (i*u + (1-i) * (1-u)) * (j*v + (1-j) * (1-v)) * (k*w + (1-k) * (1-w)) *
-                        rand[ perm[0][(x+i) & 255] ^ perm[1][(y+j) & 255] ^ perm[2][(z+k) & 255] ];
+                {
+                    vec3 weight{u - i, v - j, w - k};
+                    auto c = rand[ perm[0][(x+i) & 255] ^ perm[1][(y+j) & 255] ^ perm[2][(z+k) & 255] ];
+
+                    acc += (i*uu + (1-i)*(1-uu)) * (j*vv + (1-j)*(1-vv)) * (k*ww + (1-k)*(1-ww)) * dot(c, weight);
+                }
 
         return acc;
     }
 
 private:
     int perm[3][256];
-    double rand[256];
+    vec3 rand[256];
 };
 
 #endif
