6fd0be66f2f5317536495a33de6924c6930c54bf
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2024-03-03 23:17:18 -0500

    Adjusting the phase

--- noise.hpp
+++ noise.hpp
@@ -5,13 +5,16 @@
 #include "perlin.hpp"
 #include "texture.hpp"
 
+#include <cmath>
+
 struct noise : texture
 {
     explicit noise(double scale = 1) : scale{scale} { }
 
     virtual color3 value(const point2&, const point3& p) const override
     {
-        return color3{1, 1, 1} * gen.turb(scale * p, 7);
+        auto s = scale * p;
+        return color3{.5, .5, .5} * (1 + std::sin(s.z() + 10 * gen.turb(s, 7)));
     }
 
 private:
