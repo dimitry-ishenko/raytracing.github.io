ad6ddc27297c12f8ba99a6e0b08152acf6537422
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2024-03-25 12:39:32 -0400

    Add box() function

---
+++ box.hpp
@@ -0,0 +1,33 @@
+#ifndef BOX_HPP
+#define BOX_HPP
+
+#include "material.hpp"
+#include "object.hpp"
+#include "point.hpp"
+#include "quad.hpp"
+
+#include <cmath>
+
+inline auto box(const point3& a, const point3& b, std::shared_ptr<material> mat)
+{
+    auto [x0, x1] = std::minmax({ a.x(), b.x() });
+    auto [y0, y1] = std::minmax({ a.y(), b.y() });
+    auto [z0, z1] = std::minmax({ a.z(), b.z() });
+
+    vec3 dx{x1 - x0, 0, 0};
+    vec3 dy{0, y1 - y0, 0};
+    vec3 dz{0, 0, z1 - z0};
+
+    object_list box;
+
+    box.push_back(std::make_shared<quad>( point3{x0, y0, z1},  dx,  dy, mat ));
+    box.push_back(std::make_shared<quad>( point3{x1, y0, z1}, -dz,  dy, mat ));
+    box.push_back(std::make_shared<quad>( point3{x1, y0, z0}, -dx,  dy, mat ));
+    box.push_back(std::make_shared<quad>( point3{x0, y0, z0},  dz,  dy, mat ));
+    box.push_back(std::make_shared<quad>( point3{x0, y1, z1},  dx, -dz, mat ));
+    box.push_back(std::make_shared<quad>( point3{x0, y0, z0},  dx,  dz, mat ));
+
+    return box;
+}
+
+#endif
