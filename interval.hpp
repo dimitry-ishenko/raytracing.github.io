60f19267e94a127befffd07592f360dc6fb99f11
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2024-03-25 20:12:38 -0400

    interval: add function sorted()

--- interval.hpp
+++ interval.hpp
@@ -1,6 +1,7 @@
 #ifndef INTERVAL_HPP
 #define INTERVAL_HPP
 
+#include <algorithm>
 #include <limits>
 
 constexpr const auto inf = std::numeric_limits<double>::infinity();
@@ -26,6 +27,12 @@ constexpr auto padded(interval in)
     constexpr auto p = .0001;
     if (len(in) < p) in.min -= p / 2, in.max += p / 2;
     return in;
+}
+
+constexpr auto sorted(double m, double n)
+{
+    auto [min, max] = std::minmax(m, n);
+    return interval{min, max};
 }
 
 constexpr auto surr(const interval& in, double t) { return in.min < t && t < in.max; }
