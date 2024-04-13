a71be9f12ab6fbd7a94e10f84ed3cbda1629cc00
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2024-04-12 22:43:32 -0400

    color3: constexpr from_vec3()

--- color.hpp
+++ color.hpp
@@ -10,7 +10,7 @@ struct color3 : array<double, 3>
     constexpr auto g() const { return data()[1]; }
     constexpr auto b() const { return data()[2]; }
 
-    static auto from_vec3(const vec3& v) { return color3{v.x(), v.y(), v.z()}; }
+    static constexpr auto from_vec3(const vec3& v) { return color3{v.x(), v.y(), v.z()}; }
 };
 
 #endif
